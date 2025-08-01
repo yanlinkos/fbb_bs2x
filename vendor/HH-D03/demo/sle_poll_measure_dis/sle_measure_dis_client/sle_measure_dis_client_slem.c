e833686ff04d2aa955e01163b83eace0fa90229d
温老师 <1161625498@qq.com>
2025-08-01 14:44:45 +0800

    add Lierda

--- vendor/HH-D03/demo/sle_poll_measure_dis/sle_measure_dis_client/sle_measure_dis_client_slem.c
+++ vendor/HH-D03/demo/sle_poll_measure_dis/sle_measure_dis_client/sle_measure_dis_client_slem.c
@@ -11,7 +11,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */
 #include "sle_measure_dis_client.h"
 #include "sle_errcode.h"
 #include "sle_common.h"
